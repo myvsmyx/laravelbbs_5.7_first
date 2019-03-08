a8db97f66c2981682827b54f230206f049f47c7c
vagrant <vagrant@homestead>
2019-03-08 16:40:57 +0000

    上传头像功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
-    public function show(User $user) 
+    public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
@@ -18,9 +19,16 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        if( $request->avatar ) {
+            $result = $uploader->save($request->avatar, 'avatar', $user->id);
+            if ( $result ) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
